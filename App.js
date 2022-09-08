cd97c90c17b4f45bf96b8905b2dfa2a14104c888
Trinity Meyers <trimeyers1@gmail.com>
2022-09-08 00:52:49 -0500

    init commit

--- App.js
+++ App.js
@@ -1,14 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  View
+} from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import concert from './assets/concert.png';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const App = () => {
+
+  const [Tickets, setTickets] = useState(null);
+  const [TotalTicketsCost, setTotalTicketsCost] = useState(null);
+  const [TotalTicketsCostText, setTotalTicketsCostText] = useState("");
+  const [TicketCost, setTicketCost] = useState(99.99);
+
+  const Title = () => {
+    return (
+      <View>
+        <Text>Ticket Vault</Text>
+      </View>
+    );
+  }
+
+  const TicketInput = () => {
+    const [text, setText] = useState('');
+
+    return (
+      <View>
+        <TextInput
+          placeholder="Number of Tickets"
+          onChangeText={newText => setText(newText)}
+          defaultValue={text} />
+      </View>
+    );
+  }
+  
+  function CalcButton() {
+
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={setTotalTicketsCost(Tickets * TicketCost)}>
+          <Text>Find The Cost</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  const ResultText = () => {
+
+    setTotalTicketsCostText("Ticket Cost: " + TotalTicketsCost)
+
+    if (TotalTicketsCost != null) {
+      return (
+        <View>
+          <Text>{TotalTicketsCostText}</Text>
+        </View>
+      );
+    }
+  }
+
+  const ConcertImage = () => {
+    return (
+      <View>
+        <Image source={concert} style={{ height: 200, width: 500 }} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title />
+      <TicketInput />
+      <CalcButton />
+      <ResultText />
+      <ConcertImage />
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +94,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    textAlign: 'center',
+    backgroundColor: 'pink',
+    padding: 20,
+    borderRadius: 5,
   },
 });
