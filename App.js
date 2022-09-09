118276d43f3331338d64747101991aaf8da50fd9
Trinity Meyers <tm668536@southeast.edu>
2022-09-09 12:35:39 -0500

    works on snack

--- App.js
+++ App.js
@@ -11,30 +11,28 @@ import * as SplashScreen from 'expo-splash-screen';
 import concert from './assets/concert.png';
 
 export default function App() {
-
   const [Tickets, setTickets] = useState(null);
-  const [TotalTicketsCostText, setTotalTicketsCostText] = useState("");
-  const [Text, setText] = useState("");
+  const [TotalTicketsCostText, setTotalTicketsCostText] = useState('');
   const [TicketCost, setTicketCost] = useState(99.99);
 
   function onPress() {
-    setTotalTicketsCostText("Ticket Cost: " + Tickets * TicketCost)
+    setTotalTicketsCostText('Ticket Cost: ' + Tickets * TicketCost);
   }
 
   return (
     <View style={styles.container}>
-        <Text>Ticket Vault</Text>
-        <TextInput
-          placeholder="Number of Tickets"
-          onChangeText={newText => setTickets(newText)}
-          defaultValue={Tickets} />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={onPress}>
-          <Text>Find The Cost</Text>
-        </TouchableOpacity>
-        <Text>{TotalTicketsCostText}</Text>
-        <Image source={concert} style={styles.img} />
+      <Text style={styles.title}>Ticket Vault</Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="Number of Tickets"
+        onChangeText={(newText) => setTickets(newText)}
+        defaultValue={Tickets}
+      />
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttontext}>Find The Cost</Text>
+      </TouchableOpacity>
+      <Text style={styles.results}>{TotalTicketsCostText}</Text>
+      <Image source={concert} style={styles.img} />
     </View>
   );
 }
@@ -46,14 +44,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  img: {
-    height: 200,
-    width: 500
+  title: {
+    fontSize: 45,
+    padding: 20,
+  },
+  textinput: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  results: {
+    fontSize: 20,
+    padding: 10,
   },
   button: {
     textAlign: 'center',
     backgroundColor: 'pink',
-    padding: 20,
+    padding: 10,
     borderRadius: 5,
+  },
+  buttontext: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  img: {
+    height: 200,
+    width: 500,
   },
 });
